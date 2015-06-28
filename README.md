7f5b1c0a88143fb3a7108e564c8c19c6d95ae91b
Anderson <enfridwe@gmail.com>
2015-06-28 18:12:45 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+#glass
+Start new project
+```bash
+$ glass init
+```
+Build Project - it will update `site` folder with the new build.
+```bash
+$ glass render
+```
+##default.json
+With this file you can set the position of files and set any global variable.
+```javascript
+{
+    "theme": "templates", //theme folder
+    "name": "", //name of the Site
+    "title": "", //title of the site.
+    "base": "", //base url from project
+    "positions": [], //set prosition of files which come first and which come at last. 
+    "variables": {} //set xml variables such as <icon> and get the value setted as an variable on the templating system.
+}
+```
+###Positions example
+```javascript
+ {
+ 	"positions": ["index.md", "about.md", "how.md"]
+ }
+```
+The first element rendered it will be `index.md` after that `about.md` and so on...
+The `{{position}}` object on template it will be `0` for `index.md` and `1` for `about.md` and at last `2` for `how.md`.
+###Variables example
+```javascript
+ {
+ 	"variables": {
+    	"h1": "title" 
+    }
+ }
+```
+The `<h1>` element it will be saved as a variable `{{title}}` on the templating system. Remember the `<h1>` element it will be removed from the markdown file.
+##Templating
+All variables from `default.json` can be accessed by `site` object over the template.
+
+###Example
+```xml
+<title>{{site.name}} - {{site.title}}</title>
+```
+
+###All objects:
+```xml
+{{content}} <!-- get content from markdown -->
+{{site}} <!-- get object from default.json -->
+{{position}} <!-- get position from rendering example if index.md it's the first to be rendered the position will be 0 -->
+{{list}} <!-- it will return an array with all markdown files precompiled in html. -->
+```
---
+++ app.js
@@ -0,0 +1,107 @@
+var markdown = require('marked');
+var nunjucks = require('nunjucks');
+var mkpath = require('mkpath');
+var path = require('path');
+var list = require('dirs');
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+var getDefaultJsonObject = function() {
+	var j = {
+  "theme": "templates",
+  "name": "",
+  "title": "",
+  "base": "",
+  "positions": [],
+  "variables": {}
+}
+	return JSON.stringify(j, null, 4);
+}
+var where = function(array, query) {
+	return array.indexOf(query);
+}
+var _folder = process.cwd();
+
+if(process.argv.indexOf('init') != -1) {
+	mkpath.sync(path.join(_folder, './site'), 0700);
+	mkpath.sync(path.join(_folder, './templates'), 0700);
+	console.log('Created folders');
+	fs.writeFileSync(path.join(_folder, 'default.json'), getDefaultJsonObject(), 'utf8');
+	console.log('Created default.json');
+	return false;
+}
+if(process.argv.indexOf('render') == -1) {
+	return false;
+}
+nunjucks.configure(path.join(_folder, './templates/'), { autoescape: false });
+if(fs.existsSync(path.join(_folder, 'default.json')))
+	var _glassFile = JSON.parse(fs.readFileSync(path.join(_folder, 'default.json')).toString());
+else
+	var _glassFile = {};
+//START FROM SYSTEM
+//CREATE LIST OF MARKDOWN FILES
+var _markdown = [];
+var addMdown = function(obj, where) {
+	if(where == -1) {
+		return _markdown.push(obj);
+	} else {
+		_markdown[where] = obj;
+		return where+1;
+	}
+};
+console.log('Starting on Folder', _folder);
+var _listOfFiles = list.sync(_folder);
+//console.log('listing files', _listOfFiles);
+//SEARCH FOR MARKDOWN FILES
+var  i, _l = _listOfFiles.length;
+for(i = 0; i < _l; i++) {
+	//read file and save on _markdown
+	var _file = _listOfFiles[i];
+	var fileType = _file.split('.').pop().toLowerCase();
+	if(fileType == 'md') {
+		var newFile = _file.split(_folder)[1].split('.');
+		newFile.pop();
+		newFile = newFile.join('.').replace(/\\/g,"/");
+		var realFile = newFile+'.md';
+		newFile += "/";
+		if(newFile.toLowerCase() == '/index/') {
+			newFile = '/index.html';
+		}
+		console.log('Starting with ', newFile);
+		//SAVE AS OBJECT
+		var id = addMdown({
+			file: newFile,
+			content: markdown( fs.readFileSync(_file).toString() )
+		}, where(_glassFile.positions, realFile));
+		var $ = cheerio.load(_markdown[id-1].content);
+		for(var key in _glassFile.variables){
+			var newNameVariable = _glassFile.variables[key];
+			_markdown[id-1][newNameVariable] = $(key).eq(0).text();
+			$(key).eq(0).remove();
+		}
+		_markdown[id-1].content = $.html();
+		console.log('Loaded MD', newFile);
+	}
+}
+
+//RENDER FILES
+//GET THEME
+_glassFile.theme = path.join(_folder, _glassFile.theme, 'index.html');
+//START RENDERING THEME.
+for(var i = 0; i < _markdown.length; i++) {
+	var render = _markdown[i];
+	console.log('RENDERING MDOWN FILE', render.file);
+	//GET ALL FILES
+	render.list =  _markdown;
+	render.site = _glassFile;
+	render.position = i;
+	var res = nunjucks.render(_glassFile.theme, render);
+	var _file = path.join(_folder, 'site', (render.file == '/index.html' ? "" : render.file));
+	//CREATE FOLDER
+	if (render.file != '/index.html')
+		mkpath.sync(_file, 0700);
+	//SAVE FILE
+	fs.writeFileSync(path.join(_file, 'index.html'), res, 'utf8');
+}
+console.log('DONE');
+process.exit(0);
